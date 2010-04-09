d91b549767b70db4d70e64749ba2279a34cc7889
Dan Barry <dan@scribd.com>
2010-04-09 01:11:39 -0500

    fresh rails

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_foo_session',
+  :secret      => '2822112408cfd7e43969701286fa3d72bea882b35a610216f287733b147c678e0be77eac87ec0d655e7daf0b37820928b1ee6f2f24d8af4f897673fd33d31c0c'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
