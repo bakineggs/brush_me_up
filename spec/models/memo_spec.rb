fc8e6d8af3d436ee0fbcb3eecf833b8c65ddfa8a
Dan Barry <dan@scribd.com>
2010-04-14 16:04:34 -0500

    fuck hiding internals as long as shit works

--- spec/models/memo_spec.rb
+++ spec/models/memo_spec.rb
@@ -40,24 +40,4 @@ describe Memo do
       end
     end
   end
-
-  describe 'hiding internals' do
-    before :each do
-      @memo = Memo.new @valid_attributes
-    end
-
-    it 'should not allow modification of repeat_at' do
-      lambda { @memo.repeat_at = Time.now }.should raise_error
-    end
-
-    it 'should not expose interval' do
-      lambda { @memo.interval }.should raise_error
-      lambda { @memo.interval = 1.day }.should raise_error
-    end
-
-    it 'should not expose learning_rate' do
-      lambda { @memo.learning_rate }.should raise_error
-      lambda { @memo.learning_rate = 1 }.should raise_error
-    end
-  end
 end
